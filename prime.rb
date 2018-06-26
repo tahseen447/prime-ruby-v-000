3992a092565859ea0acf764a27d8842b77fb7f34
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-06-26 21:17:29 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,10 +3,10 @@ def prime?(number)
   if number <=1
     return false
   end
-  if number === 2 || number === 3 
+  if number === 2 || number === 3
     return true
   end
-  
+  count =2
   while count < number
     if(number % count  === 0)
       return false
