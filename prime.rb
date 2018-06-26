e01e6dc37a7ad378036a1d1af9cac93b2d31dfe2
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-06-26 21:14:04 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,3 +1,17 @@
 # Add  code here!
 def prime?(number)
+  if number <=1
+    return false
+  end
+  if num ===2
+    return false
+  end
+  count = 2
+  while count < number
+    if(number % count  === 0)
+      return false
+    end
+    count +=1
+  end
+  return false
 end
